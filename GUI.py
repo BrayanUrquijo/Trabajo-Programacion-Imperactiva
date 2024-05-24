2f6dd13c21b5687b92ee7046171e082a47769005
Brayan Urquijo <brayan.urquijo@correounivalle.edu.co>
2024-05-24 16:02:09 -0500

    Logo And Windows

--- GUI.py
+++ GUI.py
@@ -6,11 +6,15 @@
 # Proyecto Final
 
 import customtkinter as ctk
+from tkinter import PhotoImage
 
 root = ctk.CTk()
+logo = PhotoImage(file="SuperFly.png")
+
 root.title("Super Fly")
-root.geometry("500x600")
+root.geometry("500x600+350+200")
 root.resizable(0, 0)
 root.iconbitmap("SuperFly.ico")
+
 root.mainloop()
 
