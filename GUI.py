50326e2abb8aff46cb1f4621129c60f47c95318e
Brayan Urquijo <brayan.urquijo@correounivalle.edu.co>
2024-05-24 15:25:47 -0500

    Root Update

--- GUI.py
+++ GUI.py
@@ -1,3 +1,12 @@
+# Integrante1: PrimerNombre SegundoApellido – código1
+# Integrante2: PrimerNombre SegundoApellido – código2
+# Integrante2: PrimerNombre SegundoApellido – código3
+# Docente: Luis Germán Toro Pareja
+# Número de grupo:
+# Proyecto Final
+
 import customtkinter as ctk
-vos mas 
-# si perro hp
+
+root = ctk.CTk()
+root.title("")
+root.mainloop()
