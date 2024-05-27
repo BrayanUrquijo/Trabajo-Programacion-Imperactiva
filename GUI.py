78dc85f70aa58fa2c4fccc94d094ff7a9e6086bf
Brayan Urquijo <brayan.urquijo@correounivalle.edu.co>
2024-05-27 15:14:02 -0500

    Actilizacion De Customtkinter

--- GUI.py
+++ GUI.py
@@ -5,13 +5,15 @@
 # Número de grupo:
 # Proyecto Final
 
-import customtkinter as ctk
+from customtkinter import *
 from tkinter import PhotoImage
 
-ctk.set_appearance_mode("system")
+set_appearance_mode("system")
 
-root = ctk.CTk()
+root = CTk()
+
 #caracteristicas
+
 root.title("Super Fly")
 root.geometry("500x600+350+150")
 root.resizable(0, 0)
